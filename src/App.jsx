b01ac43e4d556ac354e99698451799275f7bb890
sazid0101 <shahriarislamsazid@gmail.com>
2025-03-19 18:54:58 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      
+      <h1 className='text-6xl bg-green-600'>Vite + React</h1>
+      
+    </>
+  )
+}
+
+export default App
