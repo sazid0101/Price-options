255376bebfcc08fec7c94809da028eec4d132611
sazid0101 <shahriarislamsazid@gmail.com>
2025-03-21 03:46:15 +0600

    design card

--- src/Components/PriceOption/PriceOption.jsx
+++ src/Components/PriceOption/PriceOption.jsx
@@ -1,14 +1,16 @@
+import { FaCheckCircle } from "react-icons/fa";
 const PriceOption = ({option}) => {
     const {name,features,price} = option
     return (
         <div className="text-white">
             
-            <div className="bg-teal-500 p-5 rounded-lg">
-            <h1 className="text-xl font-bold ">{name}</h1>
+            <div className="bg-teal-500 p-5 rounded-lg ">
+            <h1 className="text-xl font-bold  text-center pb-1">{name}</h1>
+            <div className="border-t-2  border-gray-200 pb-3"></div>
             {
-                features.map(feature =><li>{feature}</li> )
+                features.map(feature =><p className="flex items-center gap-2"><FaCheckCircle className="text-purple-700"></FaCheckCircle> {feature}</p> )
             }
-            <h1 className="font-bold">Price: {price}</h1>
+            <h1 className="font-bold pt-2">Price: {price}</h1>
             </div>
         </div>
     );
