6265b0a1b9c4465fcff2b897b97b2fbfe818910d
sazid0101 <shahriarislamsazid@gmail.com>
2025-03-20 17:47:45 +0600

    responsive navbar added for medium device & small device

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import Link from "../../Link/Link";
+import { BiMenuAltLeft } from "react-icons/bi";
+import { IoClose } from "react-icons/io5";
 
 const NavBar = () => {
+
+    const [open, setOpen] = useState(false)
 
     const navRoutes = [
         { id: 1, path: "/", name: "Home" },
@@ -11,8 +16,16 @@ const NavBar = () => {
       ];
 
     return (
-        <nav>
-           <ul className="md:flex">
+        <nav className="bg-slate-300 pt-4 pb-5">
+            <div className="md:hidden" onClick={()=> setOpen(!open)}>
+                {
+                    open === true?
+                    <IoClose className="text-3xl"></IoClose>:
+                    <BiMenuAltLeft className="text-3xl"></BiMenuAltLeft>
+                }
+            
+            </div>
+           <ul className={`md:flex duration-1000 absolute bg-none md:static text-white rounded-lg pl-5   ${open? "top-16 bg-green-500" : "-top-60"} text-black`}>
            {
                 navRoutes.map(route => <Link route={route}></Link>)
             }
