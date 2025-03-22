b137b4304fba495e4584cb3e951803e750878892
sazid0101 <shahriarislamsazid@gmail.com>
2025-03-22 15:14:45 +0600

    button

--- src/Components/PriceOption/PriceOption.jsx
+++ src/Components/PriceOption/PriceOption.jsx
@@ -2,15 +2,21 @@ import { FaCheckCircle } from "react-icons/fa";
 const PriceOption = ({option}) => {
     const {name,features,price} = option
     return (
-        <div className="text-white ">
+        <div className="text-white bg-teal-500 p-5 rounded-lg flex flex-col">
             
-            <div className="bg-teal-500 p-5 rounded-lg ">
+            <div className="flex-grow ">
             <h1 className="text-xl font-bold  text-center pb-1">{name}</h1>
             <div className="border-t-2  border-gray-200 pb-3"></div>
-            {
+            <div className="pl-5 flex-grow">
+                {
                 features.map(feature =><p className="flex items-center gap-2"><FaCheckCircle className="text-purple-700"></FaCheckCircle> {feature}</p> )
-            }
+                }
+            </div>
             <h1 className="font-bold pt-2">Price: {price}</h1>
+            </div>
+
+            <div className="justify-center pt-8">
+               <button className="font-bold bg-green-600 w-full hover:bg-green-900 rounded-lg">Purchase</button>
             </div>
         </div>
     );
